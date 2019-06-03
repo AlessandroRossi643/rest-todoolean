2f9ac9109812abe8d65c19af234f73d75e9f1e96
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-06-03 21:25:12 +0200

    2. Eliminazione di un todo

--- script.js
+++ script.js
@@ -10,6 +10,36 @@ $(document).ready(function(){
     }
   });
 
+  $(document).on({
+    mouseenter:function(){
+      $(this).children().show("drop");
+    },
+    mouseleave:function(){
+      $(this).children().hide("drop");
+    }
+  },".todo");
+
+  $(document).on("click",".todo i",function(){
+    var iddaEliminare=$(this).attr("data-id");
+    console.log(iddaEliminare);
+    eliminaImpegno(urlBase,iddaEliminare);
+  });
+
+
+// Function Elimina impegno
+  function eliminaImpegno(url,id){
+    $.ajax({
+      url: url+"/"+id,
+      method: "DELETE",
+      success: function(){
+        stampaLista();
+      },
+      error:function(){
+        alert("Ops, qualcosa è andato storto");
+      }
+    });
+  }
+
 
 // Function Crea Impegno
   function creaImpegno(url,nuovoimpegno){
@@ -31,19 +61,22 @@ $(document).ready(function(){
 
 // Function Stampa Lista
   function stampaLista(){
+    $('#nuovoimpegno').val('');
     $('#lista').html('');
-
     $.ajax({
       url: urlBase,
       method: "GET",
       success: function(data){
 
         for (var i = 0; i < data.length; i++) {
+          var idImpegno=data[i].id;
           var descrizioneImpegno= data[i].text;
           console.log(descrizioneImpegno);
+          console.log(idImpegno);
 
           var compilazione={
-            'impegno': descrizioneImpegno
+            'impegno': descrizioneImpegno,
+            'id': idImpegno
           }
           $('#lista').append(template_funzione(compilazione));
         }
